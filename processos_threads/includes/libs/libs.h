42ee4038dea8837fcc7eb60e9e2626d17250f387
Matheus <matheusweinfurter@gmail.com>
2026-09-03 19:56:28 -0300

    Add:'includes' directory and common libs

---
+++ processos_threads/includes/libs/libs.h
@@ -0,0 +1,14 @@
+#ifndef LIBS_H
+#define LIBS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/mman.h>
+
+#endif
