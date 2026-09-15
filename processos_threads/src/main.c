d67f9718c962259860d87d5f45e4cd391fbd005c
Matheus <matheusweinfurter@gmail.com>
2026-09-15 20:27:18 -0300

    Update: I started writing the code to share the same address between different processes using mutexes.

--- processos_threads/src/main.c
+++ processos_threads/src/main.c
@@ -12,10 +12,54 @@ para processar os dados.
 */
 
 #define TAMANHO_BUFFER 15
+
+typedef struct{
+    int *buffer;
+    int counter;
+    pthread_mutex_t mutex;
+    pthread_mutexattr_t attr;
+}shared_mem;
+
+void consumer(int *buffer, int *counter){
+    if(*counter > 0){
+        for(;*counter >= 0; (*counter)--){
+            printf("Consumer: %d\n", buffer[*counter]);
+            buffer[*counter] = 0;
+            printf("Consumer: %d\n", buffer[*counter]);
+        }
+    }
+}
+
+void producer(int *buffer, int *counter){
+    if(*counter == 0){
+        for(;*counter < TAMANHO_BUFFER; (*counter)++){
+            printf("counter %d\n", *counter);
+            buffer[*counter] = *counter + 5;
+            printf("Producer: %d\n", buffer[*counter]);
+        }
+    }
+}
+
 int main(){
     int i;
-    int buffer[15];
-    int *ptr = mmap
+    shared_mem *ptr;
+    shared_mem shared;
+    shared.counter = 0;
+
+    pthread_mutexattr_init(&shared.attr);
+    pthread_mutexattr_setpshared(&shared.attr, PTHREAD_PROCESS_SHARED);
+    pthread_mutex_init(&shared.mutex, &shared.attr);
+    pthread_mutexattr_destroy(&shared.attr);
+
+    ptr = mmap
+        (NULL,
+        sizeof(shared_mem),
+        PROT_READ | PROT_WRITE,
+        MAP_SHARED | MAP_ANONYMOUS,
+        -1,
+        0);
+
+    shared.buffer = mmap
         (NULL,
         TAMANHO_BUFFER * sizeof(int),
         PROT_READ | PROT_WRITE,
@@ -23,9 +67,20 @@ int main(){
         -1,
         0);
 
-    for(i = 0; i < TAMANHO_BUFFER; i++){
-        ptr[i] = i * 3;
-        printf("Num: %d\n", ptr[i]);
+    pid_t proc = fork();
+
+    while(true){
+        if(proc == 0){
+            pthread_mutex_lock(&shared.mutex);
+            producer(shared.buffer, &shared.counter);
+            pthread_mutex_unlock(&shared.mutex);
+        }
+
+        if(proc != 0){
+            pthread_mutex_lock(&shared.mutex);
+            consumer(shared.buffer, &shared.counter);
+            pthread_mutex_unlock(&shared.mutex);
+        }
     }
 
     return 0;
